a17a5b9317d50df026ef307b44455dee89d78b49
aitay721822 <aitay721822@gmail.com>
2019-12-30 23:18:23 +0800

    Update Settings.py

--- Repository/Settings.py
+++ Repository/Settings.py
@@ -14,7 +14,7 @@ defaultParseTag = True
 defaultExtras = "can_comment,count_comments,count_faves,description,isfavorite,license,media,needs_interstitial,owner_name,path_alias,realname,rotation,url_sq,url_q,url_t,url_s,url_n,url_w,url_m,url_z,url_c,url_l"
 defaultLang = "lang=zh-Hant-HK"
 SearchMethod = "flickr.photos.search"
-api_key = "11481d12188aab278f01b6041b3d07e3"
+api_key = "-- insert your api key here --"
 defaultFormat = "json"
 
 resolution = ['url_l','url_c','url_z','url_m','url_w','url_n','url_s','url_t','url_q','url_sq']
