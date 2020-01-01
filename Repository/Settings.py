be598f383b46be4ff3b55445c8f38d5292abe719
aitay721822 <aitay721822@gmail.com>
2020-01-01 19:04:59 +0800

    Update Settings.py

--- Repository/Settings.py
+++ Repository/Settings.py
@@ -1,8 +1,15 @@
+#
+# Flickr Default Url & PREFIX
+#
 baseUrl = "https://www.flickr.com"
 baseApiUrl = "https://api.flickr.com/services/rest"
 photoPrefix = "photos"
 sizePrefix = "sizes/o"
 
+#
+# Flickr Default Search Settings. 
+# DO NOT CHANGE THIS
+#
 defaultnojsonCallback =True
 defaultHerMasClient = True
 defaultHerMas = True
@@ -14,7 +21,14 @@ defaultParseTag = True
 defaultExtras = "can_comment,count_comments,count_faves,description,isfavorite,license,media,needs_interstitial,owner_name,path_alias,realname,rotation,url_sq,url_q,url_t,url_s,url_n,url_w,url_m,url_z,url_c,url_l"
 defaultLang = "lang=zh-Hant-HK"
 SearchMethod = "flickr.photos.search"
-api_key = "-- insert your api key here -- "
 defaultFormat = "json"
 
+#
+# API_KEY : Use it to access Flickr site
+#
+api_key = "-- insert your api key here -- "
+
+#
+# DO NOT CHANGE THIS
+#
 resolution = ['url_l','url_c','url_z','url_m','url_w','url_n','url_s','url_t','url_q','url_sq']
